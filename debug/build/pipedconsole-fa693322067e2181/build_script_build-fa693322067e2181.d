58b8c51463c6194e90ea228092e7e18c2d2ea01d
foxcirc <whyfoxcirc@gmx.de>
2021-05-29 22:30:07 +0200

    these two files keep appearing, what habe i done

--- debug/build/pipedconsole-fa693322067e2181/build_script_build-fa693322067e2181.d
+++
@@ -1,5 +0,0 @@
-C:\Users\foxcirc\3D Objects\main\projects\pipedconsole\.\debug\build\pipedconsole-fa693322067e2181\build_script_build-fa693322067e2181.exe: build.rs
-
-C:\Users\foxcirc\3D Objects\main\projects\pipedconsole\.\debug\build\pipedconsole-fa693322067e2181\build_script_build-fa693322067e2181.d: build.rs
-
-build.rs:

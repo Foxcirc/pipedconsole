969a95ce358ce6d3c79f43546ca658765722e06a
foxcirc <whyfoxcirc@gmx.de>
2021-06-06 14:32:24 +0200

    warning messages now display correctly

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
 use std::path::PathBuf;
 
-fn main() {
+fn main() -> u32 {
 
     //* cargo will call this build script again when building console_worker
     if std::env::var("CONSOLE_BUILD_SCRIPT_ALREADY_RUNNING").is_ok() { return; };
@@ -16,15 +16,19 @@ fn main() {
                 Ok(v) => v,
                 Err(e) => {
                     println!("cargo:warning=could not compile console_worker: the output of cargo did contain invalid utf-8: {}", e);
-                    String::from("error") //* trigger the other error message, since this is unusual for cargo
+                    abort!();
                 }
             };
             // todo could this be triggered by something else (like a weird path)?
             if out.contains("error") { //* an error occured
                 println!("cargo:warning=during the compilation of the console_worker executable cargo has thrown an error: {}", out)
+                abort!();
             }
         },
-        Err(err) => println!("cargo:warning=could not spawn cargo process for building the console-worker executable: {}", err)
+        Err(err) => {
+            println!("cargo:warning=could not spawn cargo process for building the console-worker executable: {}", err);
+            abort!();
+        }
     };
 
     println!("Succesfully built \"console_worker\". Target directory is {}", &out_dir);
@@ -45,14 +49,22 @@ fn main() {
             
             match std::fs::copy(&source, &dest) {
                 Ok(_) => (),
-                Err(e) => println!("cargo:warning=Could not copy {:?} to {:?}. Please make sure, that \"console_worker.exe\" is in the same directory as the executable calling it! Error message: {}", source, dest, e)
+                Err(e) => {
+                    println!("cargo:warning=Could not copy {:?} to {:?}. Please make sure, that \"console_worker.exe\" is in the same directory as the executable calling it! Error message: {}", source, dest, e)
+                    abort!();
+                }
             };
         }
 
     }
     else {
         println!("cargo:warning=Please copy \"{}\\debug\\console_worker.exe\" into the directory where your executable is located. If you want to copy automaticly please set the \"PCAUTOCOPY\" environment vriable to the destination folder For more information check out docs.rs. If you want to just disable this warning, set the \"PCAUTOCOPY\" environment variable to \"ignore\". (E.g \"set PCAUTOCOPY=path/to/your/exe/dir\" to automaticly copy or \"set PCAUTOCOPY=ignore\" to just disabe the warning) You only need to copy once at the beginning or after you changed the location of your files. Just make sure \"console_worker.exe\" is in the same directory as the executable calling it.", &out_dir);
+        abort!();
     };
     
     println!("cargo:rerun-if-env-changed=PCAUTOCOPY");    
+}
+
+macro_rules! abort {
+    () => { std::process::exit(1); }
 }
